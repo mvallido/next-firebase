1fe21a8eafe305dceeb73cc3d6657a08275bcdbe
mvallido <vallido.michael@gmail.com>
2022-05-10 21:56:30 -0700

    Google SSO

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,45 +1,44 @@
-import Link from 'next/link'
-
+import Link from 'next/link';
+import { useContext } from 'react';
+import { UserContext } from '../lib/context';
+// Top navbar
 export default function Navbar() {
-    const user = true;
-    const username = true;
-    
-    return (
-        <nav className="navbar">
-            <ul>
-                <li>
-                    <Link href="/">
-                        <button className="btn-logo">FEED</button>
-                    </Link>
-                </li>
+    const { user, username } = useContext(UserContext)
 
-                {/* user is signed-in and has username */}
-                {username && (
-                    <>
-                        <li>
-                            <Link href="admin">
-                                <button className="btn-blue">Write Posts</button>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href={`/${username}`}>
-                                <img src={user?.photoURL} />
-                            </Link>
-                        </li>
-                    </>
-                )}
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button className="btn-logo">FEED</button>
+          </Link>
+        </li>
 
-                 {/* user is not signed-in OR has not created a username */}
-                 {!username && (
-                    <li>
-                        <Link href="/enter">
-                            <button className="btn-blue">Log in</button>
-                        </Link>
-                    </li>
-                )}
+        {/* user is signed-in and has username */}
+        {username && (
+          <>
+            <li className="push-left">
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <img src={user?.photoURL} />
+              </Link>
+            </li>
+          </>
+        )}
 
-
-            </ul>
-        </nav>
-    )
+        {/* user is not signed OR has not created username */}
+        {!username && (
+          <li>
+            <Link href="/enter">
+              <button className="btn-blue">Log in</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
 }
--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,42 @@
-export default function EnterPage({ }) {
+import { auth, firestore, googleAuthProvider } from '../lib/firebase';
+import { UserContext } from '../lib/context';
+
+import { useEffect, useState, useCallback, useContext } from 'react';
+import debounce from 'lodash.debounce';
+
+export default function Enter(props) {
+  const { user, username } = useContext(UserContext);
+
+  // 1. user signed out <SignInButton />
+  // 2. user signed in, but missing username <UsernameForm />
+  // 3. user signed in, has username <SignOutButton />
   return (
     <main>
-        <h1>Hello, Sign up!</h1>
+      {user ? !username ? <UsernameForm /> : <SignOutButton /> : <SignInButton />}
     </main>
-  )
+  );
+}
+
+// Sign in with Google button
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+  };
+
+  return (
+      <button className="btn-google" onClick={signInWithGoogle}>
+        <img src={'/google.png'} width="30px" /> Sign in with Google
+      </button>
+  );
+}
+
+// Sign out button
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+// Username form
+function UsernameForm() {
+  
+  return null
 }
