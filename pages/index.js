bc6a1b83940165491192811ef9c828fc73da7de9
mvallido <vallido.michael@gmail.com>
2022-05-12 23:58:13 -0700

    Completed paginated homepage and data hydration

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,70 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import PostFeed from '../components/PostFeed';
+import Loader from '../components/Loader';
+import { firestore, fromMillis, postToJSON } from '../lib/firebase';
 
-import Loader from '../components/Loader'
-import toast from 'react-hot-toast'
+import { useState } from 'react';
 
-export default function Home() {
+// Max post to query per page
+const LIMIT = 10;
+
+export async function getServerSideProps(context) {
+  const postsQuery = firestore
+    .collectionGroup('posts')
+    .where('published', '==', true)
+    .orderBy('createdAt', 'desc')
+    .limit(LIMIT);
+
+  const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+  return {
+    props: { posts }, // will be passed to the page component as props
+  };
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  // Get next page in pagination query
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+
+    const cursor = typeof last.createdAt === 'number' ? fromMillis(last.createdAt) : last.createdAt;
+
+    const query = firestore
+      .collectionGroup('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .startAfter(cursor)
+      .limit(LIMIT);
+
+    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
+
   return (
-    <div>
-      <button onClick={() => toast.success('hello!')}>Toast!</button>
-    </div>
-  )
+    <main>
+
+      <div className="card card-info">
+        <h2>💡 Next.js + Firebase Development Practice</h2>
+      </div>
+     
+      <PostFeed posts={posts} />
+
+      {!loading && !postsEnd && <button onClick={getMorePosts}>Load more</button>}
+
+      <Loader show={loading} />
+
+      {postsEnd && 'You have reached the end!'}
+    </main>
+  );
 }
